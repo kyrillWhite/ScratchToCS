c60d061485cc4a1480c3fd3b114b863fd05ec20c
kyrillWhite <kiribel2000@gmail.com>
2022-05-23 17:01:26 +0500

    feat: added transpiling warnings logger

---
+++ ScratchToCS/WarningsLogger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchToCS
+{
+    public enum WarningType
+    {
+        SeveralBlocksWhenFlagPressed,
+        UnknownBlock,
+        SeveralSprites,
+        ProceduresWithSameName,
+    }
+
+    public static class WarningsLogger
+    {
+        private static List<string> warnings = new List<string>();
+
+        public static void PushWarning(WarningType type, params object[] parameters)
+        {
+            switch(type)
+            {
+                case WarningType.SeveralBlocksWhenFlagPressed:
+                    warnings.Add($"⚠Обнаружено несколько блоков \"Когда флаг нажат\" - {parameters[0]}.");
+                    break;
+                case WarningType.UnknownBlock:
+                    warnings.Add($"⚠Обнаружен необрабатываемый блок {parameters[0]}.");
+                    break;
+                case WarningType.SeveralSprites:
+                    warnings.Add($"⚠Обнаружено несколько спрайтов - {parameters[0]}.");
+                    break;
+                case WarningType.ProceduresWithSameName:
+                    warnings.Add($"⚠Обнаружено несколько процедур с именем \"{parameters[0]}\" - {parameters[1]}.");
+                    break;
+            }
+        }
+
+        public static List<string> PopAllWarnings()
+        {
+            var allWarnings = new List<string>(warnings);
+            warnings.Clear();
+            return allWarnings;
+        }
+    }
+}
